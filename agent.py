82792ebe6d0857a2414d5d638b022093002b63df
oremaz <Coool2@users.noreply.huggingface.co>
2025-05-25 21:05:49 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -33,7 +33,7 @@ proj_llm = HuggingFaceLLM(
     tokenizer_name=model_id,
     device_map="auto",           # will use GPU if available
     model_kwargs={"torch_dtype": "auto"},
-    generate_kwargs={"temperature": 0.7, "top_p": 0.95}
+    generate_kwargs={"temperature": 0, "top_p": 0}
 )
 
 embed_model = HuggingFaceEmbedding("BAAI/bge-small-en-v1.5")
