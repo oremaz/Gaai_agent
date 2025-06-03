0d47470945737e90d07aba297e30b6f796d149f1
oremaz <Coool2@users.noreply.huggingface.co>
2025-06-03 21:00:43 +0000

    Update agent.py

--- agent.py
+++ agent.py
@@ -73,10 +73,10 @@ proj_llm = HuggingFaceLLM(
     tokenizer_name=model_id,
     device_map="auto",
     model_kwargs={"torch_dtype": "auto"},
+    max_new_tokens = 100000
     generate_kwargs={
         "temperature": 0.1,
-        "top_p": 1.0,
-        "max_new_tokens": 100000 
+        "top_p": 1.0
     }
 )
 
